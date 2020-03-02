c6f4e8c4b06907c95e388ef0456ae367f33a5589
Luiz Dendena <luiz.dendena@gmail.com>
2020-03-02 15:53:39 -0300

    adicionando comandos em variaveis

---
+++ comandos.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+listar=$(ls)
+echo $listar
--- variaveis.sh
+++ variaveis.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 var_numero=10
 var_string="Hellow World"
+
 echo $var_numero
 echo $var_string
 echo "Combinando saidas: $var_numero, $var_string"
